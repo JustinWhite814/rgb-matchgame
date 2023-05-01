beb30a19fd1ececf32811d682d93e463dffca5ac
Juswhite814 <juswhite814@gmail.com>
2023-04-30 22:29:28 -0400

    changed vars to lets

--- colorgame.js
+++ colorgame.js
@@ -18,7 +18,7 @@ function init()
 	reset();
 }
 function setupModeButtons(){
-	for(var i = 0; i < modeButtons.length; i++) {
+	for(let i = 0; i < modeButtons.length; i++) {
 		modeButtons[i].addEventListener("click",function() {
 		modeButtons[0].classList.remove("selected");
 		modeButtons[1].classList.remove("selected");
@@ -31,7 +31,7 @@ function setupModeButtons(){
 	}
 function setupSquares()
 {
-	for (var i = 0; i < squares.length; i++){
+	for (let i = 0; i < squares.length; i++){
 		// add click listeners to squares
 		squares[i].addEventListener("click",function(){
 		// grab color of square
@@ -64,7 +64,7 @@ function reset(){
 	resetButton.textContent = "New Colors";
 	messageDisplay.textContent = "";
 
-	for (var i = 0; i < squares.length ; i++) {
+	for (let i = 0; i < squares.length ; i++) {
 		if(colors[i]){
 			squares[i].style.display = "block";
 			squares[i].style.backgroundColor = colors[i];
@@ -87,7 +87,7 @@ resetButton.addEventListener("click", function()
 
 function changeColors(color){
 	// loop through all squares
-	for(var i = 0; i < squares.length; i++){
+	for(let i = 0; i < squares.length; i++){
 		// chanfe each color to match given color
 		squares[i].style.backgroundColor = color;
 	}
@@ -99,9 +99,9 @@ function pickColor(){
 }
 function generateRandomColors(num){
 	// make an array
-	var arr = []
+	let arr = []
 	// add num random colors to array
-	for (var i = 0; i < num; i++) {
+	for (let i = 0; i < num; i++) {
 	// get random color and push into array
 		arr.push(randomColor())
 		}
@@ -112,11 +112,11 @@ function generateRandomColors(num){
 function randomColor()
 {
 	// pick a red from 0 - 255
-	var r = Math.floor(Math.random() * 256);
+	let r = Math.floor(Math.random() * 256);
 	// pick a green from 0 - 255
-	var g = Math.floor(Math.random() * 256);
+	let g = Math.floor(Math.random() * 256);
 	// pick a blue from 0 - 255
-	var b = Math.floor(Math.random() * 256);
+	let b = Math.floor(Math.random() * 256);
 	
 	return "rgb(" + r + ", " + g + ", " + b + ")" ;
 }
